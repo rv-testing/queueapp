2a3f4b899fa57d960ecf58c7e6822787f9852515
venerari <venerayan@gmail.com>
2021-11-07 22:56:48 -0500

    first commit2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueueApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
